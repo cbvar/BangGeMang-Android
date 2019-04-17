aa9315e63a3ea6aade179534ce6bebfcc447ca40
cbvar <cbvar6@gmail.com>
2019-04-17 11:55:53 +0800

    禁止字体缩放

--- app/src/main/java/com/example/banggemang/base/BaseFragmentActivity.java
+++ app/src/main/java/com/example/banggemang/base/BaseFragmentActivity.java
@@ -1,6 +1,19 @@
 package com.example.banggemang.base;
 
+import android.content.Context;
+import android.content.res.Configuration;
+
 import com.qmuiteam.qmui.arch.QMUIFragmentActivity;
 
 public abstract class BaseFragmentActivity extends QMUIFragmentActivity {
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        Configuration configuration = base.getResources().getConfiguration();
+        if (configuration.fontScale != 1.0f) {
+            configuration.fontScale = 1.0f;
+            base = base.createConfigurationContext(configuration);
+        }
+        super.attachBaseContext(base);
+    }
 }
